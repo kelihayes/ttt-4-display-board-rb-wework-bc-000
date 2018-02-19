c293496279e325ed31ffc1418d1d00605649f57f
Keli Hayes <keli.hayes@wework.com>
2018-02-19 20:40:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
-board=["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board=["O", " ", "X", " ", "X", " ", " ", " ", " "]
 puts "   |   |   "
 puts "-----------"
 puts "   |   |   "
